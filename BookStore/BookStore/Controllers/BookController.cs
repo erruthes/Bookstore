9b87138d31594848cc06683b838deeb33dab5046
Eduardo Ruthes <erruthes@stefanini.com>
2020-10-14 09:21:58 -0300

    Criação de uma controller pelo visualStudio e adicionando Actions

---
+++ BookStore/BookStore/Controllers/BookController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        // /book/getallbooks
+        public string GetAllBooks()
+        {
+            return "All Books";
+        }
+
+        // /book/getbook/10
+        public string GetBook(int id)
+        {
+            return $"book with id: {id}";
+        }
+
+        // /book/searchbooks?bookName=TESTE&authorName=AUTOR
+        public string SearchBooks(string bookName, string authorName)
+        {
+            return $"Book with name: {bookName} and author: {authorName}";
+        }
+    }
+}
